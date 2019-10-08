247dca718fa2ab33fddbba6ce55121ef88fcadda
d <d@DESKTOP-85DL300>
2019-10-08 18:04:02 +0530

    Commit

---
+++ Assets/Testing/JobStudy.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Burst;
+public class JobStudy : MonoBehaviour
+{
+    [SerializeField] private bool useJobs;
+    [SerializeField] private Transform pfZombie;
+    private List<Zombie> zombieList;
+
+    public class Zombie
+    {
+        public Transform transform;
+        public float moveY;
+    }
+    private void Start()
+    {
+        zombieList = new List<Zombie>();
+        for (int i = 0; i < 1000; i++)
+        {
+            Transform zombieTransform = Instantiate(pfZombie, new Vector3(UnityEngine.Random.Range(-8f,8f), UnityEngine.Random.Range(-5f,5f)), Quaternion.identity);
+            zombieList.Add(new Zombie
+            {
+                transform = zombieTransform,
+                moveY = UnityEngine.Random.Range(1f,2f)
+            });
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float startTime = Time.realtimeSinceStartup;
+
+        foreach (Zombie zombie in zombieList)
+        {
+            zombie.transform.position += new Vector3(0, zombie.moveY * Time.deltaTime);
+            if (zombie.transform.position.y>5f)
+            {
+                zombie.moveY = -math.abs(zombie.moveY);
+            }
+            if (zombie.transform.position.y <-5f)
+            {
+                zombie.moveY = +math.abs(zombie.moveY);
+            }
+            float value = 0f;
+            for (int i = 0; i < 50000; i++)
+            {
+                value = math.exp10(math.sqrt(value));
+            }
+        }
+       /* if (useJobs)
+        {
+            NativeList<JobHandle> jobHandleList = new NativeList<JobHandle>(Allocator.Temp);
+            for (int i = 0; i < 10; i++)
+            {
+                JobHandle jobHandle = ReallyToughTaskJob();
+                jobHandleList.Add(jobHandle);
+            }
+            JobHandle.CompleteAll(jobHandleList);
+            jobHandleList.Dispose();
+        }
+        else
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ReallyToughTask();
+            }
+        }*/
+        Debug.Log(((Time.realtimeSinceStartup - startTime) * 1000f) + "ms");
+    }
+
+    private void ReallyToughTask()
+    {
+        float value = 0f;
+        for (int i = 0; i < 50000; i++) 
+        {
+            value = math.exp10(math.sqrt(value));
+        }
+    }
+    private JobHandle ReallyToughTaskJob()
+    {
+        ReallyToughJob toughJob = new ReallyToughJob();
+        return toughJob.Schedule();
+    }
+}
+[BurstCompile]
+public struct ReallyToughJob : IJob
+{
+    public void Execute()
+    {
+        float value = 0f;
+        for (int i = 0; i < 50000; i++)
+        {
+            value = math.exp10(math.sqrt(value));
+        }
+    }
+}
