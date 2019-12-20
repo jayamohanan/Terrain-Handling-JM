615a5f19faf335b9cf783f2a2368077e29a4c6a1
d <d@DESKTOP-85DL300>
2019-12-20 19:29:24 +0530

    commit

---
+++ Assets/Clouds/NoiseTextureSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseTextureSettings
+{
+    public int gridSizeA = 5;
+    public int gridSizeB = 10;
+    public int gridSizeC = 15;
+}
--- Assets/TEst/Noises.cs
+++ Assets/TEst/Noises.cs
@@ -11,7 +11,7 @@ public class Noises : MonoBehaviour
     public int resolution = 256;
     public int unitWidth;
     public int unitLength;
-    public int depth;
+    public int unitDepth;
     
     int kernel;
     ComputeBuffer ponitsBuffer;
@@ -30,17 +30,17 @@ public class Noises : MonoBehaviour
     }
     void SetComputeBuffer()
     {
-        Grid grid = new Grid(resolution, unitWidth, unitLength, depth);
+        Grid grid = new Grid(resolution, unitWidth, unitLength, unitDepth);
         Debug.Log("new grid created with "+ " resolution = "+ resolution+" unitWidth "+unitWidth+" unitLength "+unitLength);
-        Vector2[] points = grid.CreateGrid();
+        Vector3[] points = grid.Create3DGrid();
         kernel = shader.FindKernel("CSMain");
         shader.SetTexture(kernel, "Result", myRt);
-        ponitsBuffer = new ComputeBuffer(grid.rows * grid.columns * grid.depth, sizeof(float) * 2, ComputeBufferType.Default);
+        ponitsBuffer = new ComputeBuffer(grid.rows * grid.columns * grid.depth, sizeof(float) * 3, ComputeBufferType.Default);
         ponitsBuffer.SetData(points);
         shader.SetBuffer(kernel, "points", ponitsBuffer);
 
-        float[] unitDimensions = new float[] {grid.unitWidth, grid.unitLength, grid.rows, grid.columns };
-        dimensionBuffer = new ComputeBuffer(4, sizeof(float), ComputeBufferType.Default);
+        Vector3[] unitDimensions = new Vector3[] {new Vector3( grid.unitWidth, grid.unitLength, grid.unitDepth), new Vector3(grid.rows, grid.columns, grid.stacks) };
+        dimensionBuffer = new ComputeBuffer(2, sizeof(float)*3, ComputeBufferType.Default);//size along axes and cells along axis
         dimensionBuffer.SetData(unitDimensions);
         shader.SetBuffer(kernel, "unitDimensions", dimensionBuffer);
 
@@ -67,31 +67,38 @@ public class Noises : MonoBehaviour
 public class Grid
 {
     public int resolution;
+
     public int unitWidth;
     public int unitLength;
+    public int unitDepth;
+
     public int depth;
     public int rows;
     public int columns;
-    public Grid(int resolution, int unitWidth, int unitLength, int depth)
+    public int stacks;
+    public Grid(int resolution, int unitWidth, int unitLength, int unitDepth)
     {
         this.resolution = resolution;
         this.unitWidth = unitWidth;
         this.unitLength = unitLength;
-        this.depth = depth;
+        this.unitDepth = unitDepth;
+
         rows = (int)Mathf.Ceil(resolution / (float)unitLength);
         columns = (int)Mathf.Ceil(resolution / (float)unitWidth);
+        stacks = (int)Mathf.Ceil(resolution / (float)unitDepth);
+
         Debug.Log("Constructor called rows = "+rows+" columns "+columns);
     }
-    public Vector2[] CreateGrid()
+    public Vector3[] Create3DGrid()
     {
-        Vector2[] points = new Vector2[rows * columns * depth];
-        for (int k = 0; k < depth; k++)
+        Vector3[] points = new Vector3[rows * columns * depth];
+        for (int k = 0; k < stacks; k++)
         {
             for (int i = 0; i < columns; i++)
             {
                 for (int j = 0; j < rows; j++)
                 {
-                    points[k*rows*columns + i * rows + j] = new Vector2(Random.Range(i * unitWidth, (i + 1) * unitWidth), Random.Range(j * unitLength, (j + 1) * unitLength));
+                    points[k*rows*columns + i * rows + j] = new Vector3(Random.Range(i * unitWidth, (i + 1) * unitWidth), Random.Range(j * unitLength, (j + 1) * unitLength), Random.Range(k * unitDepth, (k + 1) * unitDepth));
                 }
             }
         }
